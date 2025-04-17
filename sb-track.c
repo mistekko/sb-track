3136e2caf86f6df1a2a8080a03e81be62df1ab24
mistekko <steamlie@protonmail.com>
2025-04-16 20:12:56 -0500

    Fix u8index, rename it to u8subscript and improve debugging messages
    u8index's algorithm had extremely bizarre behaviour when passed an index of 0
    (as happens once every frame cycle) since tests against index - 1 before
    decrementing index.
    To reflect that u8index is an alternative subscripting method and does not
    actually return an index, it as been renamed.
    Developers and users both would likely rather know a bit about why this program
    doesn't work, so failing "silently" on any error does not foster good UX. Thus,
    I added some error messages.
    Lastly, I corrected a use of the 'text_length' variable, which stores the byte
    length of one text cycle, in place of the number of characters in a cycle.
    This misuse of course caused some issues whenever multibyte characters were
    present.

--- sb-track.c
+++ sb-track.c
@@ -9,7 +9,6 @@
 #define FIELD_WIDTH 25
 #define PADDING_WIDTH 10
 
-
 static int
 u8strlen(const char *s)
 {
@@ -25,46 +24,53 @@ u8strlen(const char *s)
 static void
 u8putnc(const char *s, int n)
 {
-	int m = 0;
-	while (m < n) {
+	while (*s && n) {
 		putchar(*s);
 		if ((*++s & 0xC0) != 0x80)
-			m++;
+			n--;
 	}
 }
 
-static const char *
-u8index(const char *s, int i)
+const char *
+u8subscript(const char *s, int i)
 {
-	while (*s && i - 1)
-		if ((*++s & 0xC0) != 0x80)
+	while (*s++ && i)
+		if ((*s & 0xC0) != 0x80)
 			i--;
-	return s;
+	return s - 1;
 }
 
 int
 main ()
 {
 	struct mpd_connection *conn = mpd_connection_new(NULL, 0, 0);
-	if (conn == NULL)
-		exit(2);
+	if (conn == NULL) {
+		puts("[ failed to connect to MPD ]");
+		return 1;
+	}
 	if (mpd_connection_get_error(conn) != MPD_ERROR_SUCCESS) {
 		fprintf(stderr,
 			"%s\n",
 			mpd_connection_get_error_message(conn));
 		mpd_connection_free(conn);
-		exit(3);
+		puts("[ failed to connect to MPD ]");
+		return 2;
 	}
 
 	struct mpd_song *song =	mpd_run_current_song(conn);
-	if (song == NULL) /* no song is playing */
-		exit(0);
+	if (song == NULL) {
+		puts("");
+		return 0;
+	}
 	const char *artist = mpd_song_get_tag(song, MPD_TAG_ARTIST, 0);
 	const char *title  = mpd_song_get_tag(song, MPD_TAG_TITLE, 0);
 	const struct mpd_status *status = mpd_run_status(conn);
 	unsigned int secs = mpd_status_get_elapsed_time(status);
 	unsigned int secs_total = mpd_status_get_total_time(status);
-
+	if (!secs || !secs_total) {
+		puts("");
+		return 0;
+	}
 	int progress = 100 * secs / secs_total;
 
 	int info_width = u8strlen(artist) + u8strlen(title) + 3;
@@ -74,11 +80,12 @@ main ()
 		       title,
 		       progress);
 		mpd_connection_free(conn);
-		exit(0);
+		return 0;
 	}
 
 	int padding2 = FPS - (info_width + PADDING_WIDTH) % FPS;
 	int text_length = (strlen(artist)
+			   + 3
 			   + strlen(title)
 			   + PADDING_WIDTH
 			   + padding2);
@@ -93,8 +100,8 @@ main ()
 	int scroll_length = text_length * 3;
 	char *scroll = malloc(scroll_length + 1);
 	if (scroll == NULL) {
-		puts("ERROR!");
-		exit(1);
+		puts("[ memory error ]");
+		return 4;
 	}
 	scroll[scroll_length] = '\0';
 	strcpy(scroll, text);
@@ -103,12 +110,16 @@ main ()
 
 	struct timespec time;
 	if (clock_gettime(CLOCK_MONOTONIC, &time) < 0)
-		exit(1);
-	int frames = (time.tv_sec % (text_length / FPS)) * FPS;
+		puts("[ failed to get system time ]");
+	int frames = (time.tv_sec %
+		      ((info_width +
+			PADDING_WIDTH +
+			padding2) /
+		       FPS)) * FPS;
 	frames += (time.tv_nsec / 100000000) * (FPS / 10.0);
 
 	fputs("[ ", stdout);
-	u8putnc(u8index(scroll, frames), FIELD_WIDTH);
+	u8putnc(u8subscript(scroll, frames), FIELD_WIDTH);
 	printf(" ][%d%%]\n", progress);
 
 	mpd_connection_free(conn);
